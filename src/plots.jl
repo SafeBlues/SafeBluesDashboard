27e3d9d210616b65a70fb2370ecb2de1023d95e5
Thomas Graham <77311481+tjgraham@users.noreply.github.com>
2021-10-20 19:11:44 +1000

    Add function for plotting a strand's trajectory

--- src/plots.jl
+++ src/plots.jl
@@ -19,10 +19,44 @@ function ensemble_plot(strand_ids::Vector{T}) where T <: Integer
         ))
     end
 
-    layout = Layout(
-        title="Strand Trajectories",
+    layout = Layout(;
+        title="Strand Caseloads",
         xaxis_title="Time (NZST/NZDT)",
         yaxis_title="Infected Participants"
+    )
+
+    return plot(traces, layout)
+end
+
+function trajectory_plot(strand_id::Integer)
+    model = data.parameters[strand_id, :model]
+
+    times = data.strands.hourly[strand_id][:, :time_nzt]
+    traces = GenericTrace[]
+
+    # Susceptible Trace
+    susceptible = data.strands.hourly[strand_id][:, :susceptible]
+    push!(traces, scatter(;x=times, y=susceptible, line_color=BLUE, name="Susceptible"))
+
+    if model == "SEI" || model == "SEIR"
+        exposed = data.strands.hourly[strand_id][:, :exposed]
+        push!(traces, scatter(;x=times, y=exposed, line_color=ORANGE, name="Exposed"))
+    end
+
+    # Infected Trace
+    infected = data.strands.hourly[strand_id][:, :infected]
+    push!(traces, scatter(;x=times, y=infected, line_color=RED, name="Infected"))
+
+    # Recovered Trace
+    if model == "SIR" || model == "SEIR"
+        recovered = data.strands.hourly[strand_id][:, :recovered]
+        push!(traces, scatter(;x=times, y=recovered, line_color=GREEN, name="Recovered"))
+    end
+
+    layout = Layout(;
+        title="Strand $(strand_id) Trajectory",
+        xaxis_title="Time (NZST/NZDT)",
+        yaxis_title="Participants"
     )
 
     return plot(traces, layout)
